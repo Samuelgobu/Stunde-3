90f217c5b162d8e83a5b0af593b333e9cbb811ad
Joshua Roshen <joshua.roshen.smith@gmail.com>
2022-03-21 12:36:55 +0100

    yi

--- Program.cs
+++ Program.cs
@@ -10,7 +10,55 @@ namespace Stunde_3
     {
         static void Main(string[] args)
         {
-            OutOfMemory
+            //Datetypengrößen
+            short a0= short.MaxValue;
+            short a1 = short.MinValue;
+            ushort a2 = 2*short.MaxValue + 1;
+            int b = int.MaxValue;
+            long c = long.MaxValue;
+            Console.WriteLine("Short : " + a0 + " Int; " + b + " Long:" + c);
+            Console.WriteLine("Short.min : " + a1 + " Short.max: " + a0 + " ushort: " + a2);
+
+            //Verzweigungen (if - else Anweisung)
+
+            int x = 5;
+            int y = 7;
+            if (x + y > 10)
+            {
+                Console.WriteLine("Das Ergebnis ist größer als 10");
+            }
+            else if (x+y ==10)
+            {
+                Console.WriteLine("Das Ergebnis ist genau 10");
+
+            }
+            else
+            {
+                Console.WriteLine("Das Ergebnis ist kleiner al 10");
+            }
+
+            //Shleifen
+            //Wiederholte Abläufe
+
+            int zähler = 0;
+            while (zähler>-100)
+            {
+                Console.WriteLine("Der Zähler ist :" + zähler);
+                //zähler = zähler + 1;
+                // zähler +=1;
+                zähler--;
+            }
+            int zähler2 = 0;
+            do
+            {
+                Console.WriteLine("Der Zähler2 ist :" + zähler2);
+                zähler2++;
+
+
+            } while (zähler2 < 10);
+            }
+
+
+
         }
     }
-}
